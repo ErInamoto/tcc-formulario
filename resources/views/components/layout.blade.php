90333fb9fbcc87fcac43ab22d5e6d1808c786a9f
Etec Antonio Devisate <etec@devisate.com.br>
2023-09-01 21:32:25 -0300

    Html

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulário 01</title>
+
+    <link rel="stylesheet"
+        href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+
+    <link rel="stylesheet" type="text/css" href="/css/flex-style.css">
+    <link rel="stylesheet" type="text/css" href="css/flex.css">
+    <link rel="stylesheet" type="text/css" href="css/index.css">   
+</head>
+
+<body>
+    <header id="app-header" class="flex-row content-vcenter space-30 content-space-between box header">
+    <img src="/imagens/logo.png" class="logo">    
+    <h1 class="nomeTcc">Formulário</h1>
+        
+    </header>
+
+    <main class="grow">
+    {{ $slot }}
+    </main>
+
+    <footer id="app-footer" class="footer flex-row content-center box space-30">
+        <div>Feito Pelos Alunos do Técnico em informatica</div>
+    </footer>
+</body>
